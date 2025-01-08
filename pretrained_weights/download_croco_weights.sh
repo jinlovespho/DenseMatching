0f8154162d0b61917b2887104ceb0e1b787e6b5e
jinlovespho <msjchr922@gmail.com>
2025-01-08 02:04:48 +0000

    20250108 moved to server5 new start for icml2025

---
+++ pretrained_weights/download_croco_weights.sh
@@ -0,0 +1,9 @@
+
+# crocov2_vitLarge_decoderBase 
+wget https://download.europe.naverlabs.com/ComputerVision/CroCo/CroCo_V2_ViTLarge_BaseDecoder.pth
+
+# crocov2_vitBase_decoderBase 
+wget https://download.europe.naverlabs.com/ComputerVision/CroCo/CroCo_V2_ViTBase_BaseDecoder.pth
+
+# crcov2_vitBase_decoderSmall 
+wget https://download.europe.naverlabs.com/ComputerVision/CroCo/CroCo_V2_ViTBase_SmallDecoder.pth
